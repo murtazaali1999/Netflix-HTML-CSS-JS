316de64bc5e2ae523eda4543030dc00bf6645fee
Murtaza Ali <61330248+murtazaali1999@users.noreply.github.com>
2021-03-03 15:38:19 +0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 var content = document.querySelectorAll("#cont > div")
+var contentFirstChild = document.querySelector(".first-c")
 var scrolled = document.querySelectorAll(".scroll-content > div")
 
 scrolled.forEach(item=> item.addEventListener('click',selectItem));
@@ -13,17 +14,23 @@ this.classList.add("add-border");
 var cont=document.querySelector(`#${this.id}-content`);
 if(`#${this.id}-content`=="#tab-1-content"){
 
-cont.classList.add("show");
-cont.classList.remove("hide");		
-
-cont.id="tab-1-content";
+content.forEach(items=>items.classList.remove("show-grid"));
+content.forEach(items=>items.classList.remove("show"));	
+console.log(contentFirstChild);
+contentFirstChild.classList.add("f-grid","show-grid")
 
 }
 else{
 
+content.forEach(items=>items.classList.remove("show-grid"));
+content.forEach(items=>items.classList.remove("show"));
 
-cont.classList.remove("hide");		
+if(document.getElementById("tab-1-content")){
+document.querySelector('#tab-1-content').removeAttribute('id');	
+}
+cont.classList.remove("hide");	
 
+console.log(e.target)
 cont.classList.add("show");
 
 }
